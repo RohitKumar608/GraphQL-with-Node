68f4017a08327e8c3fca41c5eefda761b13cb6c6
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-08-30 13:23:51 +0530

    Deleted  math, myModule files and added gitignore

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,4 +1,36 @@
-import myAddFunction, { subtract } from './math'
+import { GraphQLServer } from "graphql-yoga"
 
-console.log(myAddFunction(1, -2))
-console.log(subtract(10, 2))
+// Type definitions(schema)
+const typeDefs =  `
+type Query {
+    title: String!
+    price: Int!
+    releaseYear: Int
+    rating: Float
+    inStock: Boolean!
+  }
+`
+// Resolvers
+
+const resolvers = {
+  Query: {
+    title() {
+      return "Its About secret"
+    },
+    price() {
+      return 54
+    },
+    releaseYear(){
+      return null
+    },
+    rating(){
+      return 3.2
+    },
+    inStock(){
+      return true
+    }
+  },
+}
+const server = new GraphQLServer({ typeDefs, resolvers })
+
+server.start(() => console.log("Server is running on localhost:4000"))
--- graphql-basics/src/math.js
+++
@@ -1,5 +0,0 @@
-const add = (a, b) => a + b
-
-const subtract = (a, b) => a - b
-
-export { add as default, subtract }
