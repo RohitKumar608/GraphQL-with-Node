f31feca5f4c0221e3bcf4824eeba6912ffe9ee2d
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-09-03 09:57:20 +0530

    Added new feature subsub pattern

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,13 +1,14 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './DB/db'
 import Query from './resolvers/Query'
 import Mutation from './resolvers/Mutation'
 import User from './resolvers/User'
 import Post from './resolvers/Post'
 import Comment from './resolvers/Comment'
+import Subscription from './Subscription/subscription'
 // Scalar types - String, Boolean, Int, Float, ID
 
-
+const pubsub = new PubSub()
 const server = new GraphQLServer({
     typeDefs: './src/Schema/schema.graphql',
     resolvers: {
@@ -15,10 +16,12 @@ const server = new GraphQLServer({
         Mutation,
         User,
         Post,
-        Comment
+        Comment,
+        Subscription
     },
     context: {
-        db
+        db,
+        pubsub
     }
 })
 
--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -39,6 +39,34 @@ const Mutation = {
 
         return deletedUsers[0]
     },
+    updateUser(parent, args, { db }, info) {
+        const { id, data } = args
+        const user = db.users.find((user) => user.id === id)
+
+        if (!user) {
+            throw new Error('User not found')
+        }
+
+        if (typeof data.email === 'string') {
+            const emailTaken = db.users.some((user) => user.email === data.email)
+
+            if (emailTaken) {
+                throw new Error('Email taken')
+            }
+
+            user.email = data.email
+        }
+
+        if (typeof data.name === 'string') {
+            user.name = data.name
+        }
+
+        if (typeof data.age !== 'undefined') {
+            user.age = data.age
+        }
+
+        return user
+    },
     createPost(parent, args, { db }, info) {
         const userExists = db.users.some((user) => user.id === args.data.author)
 
@@ -68,6 +96,28 @@ const Mutation = {
 
         return deletedPosts[0]
     },
+    updatePost(parent, args, { db }, info) {
+        const { id, data } = args
+        const post = db.posts.find((post) => post.id === id)
+
+        if (!post) {
+            throw new Error('Post not found')
+        }
+
+        if (typeof data.title === 'string') {
+            post.title = data.title
+        }
+
+        if (typeof data.body === 'string') {
+            post.body = data.body
+        }
+
+        if (typeof data.published === 'boolean') {
+            post.published = data.published
+        }
+
+        return post
+    },
     createComment(parent, args, { db }, info) {
         const userExists = db.users.some((user) => user.id === args.data.author)
         const postExists = db.posts.some((post) => post.id === args.data.post && post.published)
@@ -95,6 +145,20 @@ const Mutation = {
         const deletedComments = db.comments.splice(commentIndex, 1)
 
         return deletedComments[0]
+    },
+    updateComment(parent, args, { db }, info) {
+        const { id, data } = args
+        const comment = db.comments.find((comment) => comment.id === id)
+
+        if (!comment) {
+            throw new Error('Comment not found')
+        }
+
+        if (typeof data.text === 'string') {
+            comment.text = data.text
+        }
+
+        return comment
     }
 }
 
