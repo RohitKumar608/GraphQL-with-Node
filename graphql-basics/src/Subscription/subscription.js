04c795be4c10982355872b9361c69a8ccc32971a
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-09-03 10:30:43 +0530

    pubsub pattern added for create post and comment

--- graphql-basics/src/Subscription/subscription.js
+++ graphql-basics/src/Subscription/subscription.js
@@ -1,17 +1,30 @@
 const Subscription = {
   count: {
-      subscribe(parent, args, { pubsub }, info) {
-          let count = 0
+    subscribe(parent, args, { pubsub }, info) {
+      let count = 0
 
-          setInterval(() => {
-              count++
-              pubsub.publish('count', {
-                  count
-              })
-          }, 1000)
+      setInterval(() => {
+        count++
+        pubsub.publish("count", {
+          count,
+        })
+      }, 1000)
 
-          return pubsub.asyncIterator('count')
-      }
+      return pubsub.asyncIterator("count")
+    },
+  },
+  comment: {
+    subscribe(parent, { postId }, { pubsub, db: { posts } }, info) {
+      const findPost = posts.find((post) => post.id === postId)
+      if (!findPost) throw new Error("Post not found")
+      console.log(postId, findPost)
+      return pubsub.asyncIterator(`comment ${postId}`)
+    },
+  },
+  post: {
+    subscribe(parent, args, { pubsub }, info) {
+      return pubsub.asyncIterator(`post`)
+    },
   }
 }
 
