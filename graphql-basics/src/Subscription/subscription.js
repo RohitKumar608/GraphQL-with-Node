b349e79fa5d9e44a55d7e1d6426fc70b294713b0
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-09-04 08:47:50 +0530

    Added pubsub logic for update, delete post

--- graphql-basics/src/Subscription/subscription.js
+++ graphql-basics/src/Subscription/subscription.js
@@ -1,30 +1,19 @@
 const Subscription = {
-  count: {
-    subscribe(parent, args, { pubsub }, info) {
-      let count = 0
-
-      setInterval(() => {
-        count++
-        pubsub.publish("count", {
-          count,
-        })
-      }, 1000)
-
-      return pubsub.asyncIterator("count")
-    },
-  },
   comment: {
-    subscribe(parent, { postId }, { pubsub, db: { posts } }, info) {
-      const findPost = posts.find((post) => post.id === postId)
-      if (!findPost) throw new Error("Post not found")
-      console.log(postId, findPost)
-      return pubsub.asyncIterator(`comment ${postId}`)
-    },
+      subscribe(parent, { postId }, { db, pubsub }, info){
+          const post = db.posts.find((post) => post.id === postId && post.published)
+
+          if (!post) {
+              throw new Error('Post not found')
+          }
+
+          return pubsub.asyncIterator(`comment ${postId}`)
+      }
   },
   post: {
-    subscribe(parent, args, { pubsub }, info) {
-      return pubsub.asyncIterator(`post`)
-    },
+      subscribe(parent, args, { pubsub }, info) {
+          return pubsub.asyncIterator('post')
+      }
   }
 }
 
