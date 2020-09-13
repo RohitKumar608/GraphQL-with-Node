fb5e604cde495cf0524ce0110242eb889489a08b
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-09-13 21:35:26 +0530

    prisma configuration added from node side

---
+++ graphql-prisma/src/index.js
@@ -0,0 +1,30 @@
+import { GraphQLServer, PubSub } from 'graphql-yoga'
+import db from './DB/db'
+import Query from './resolvers/Query'
+import Mutation from './resolvers/Mutation'
+import User from './resolvers/User'
+import Post from './resolvers/Post'
+import Comment from './resolvers/Comment'
+import Subscription from './Subscription/subscription'
+// Scalar types - String, Boolean, Int, Float, ID
+
+const pubsub = new PubSub()
+const server = new GraphQLServer({
+    typeDefs: './src/Schema/schema.graphql',
+    resolvers: {
+        Query,
+        Mutation,
+        User,
+        Post,
+        Comment,
+        Subscription
+    },
+    context: {
+        db,
+        pubsub
+    }
+})
+
+server.start(() => {
+    console.log('The server is up!')
+})
