4d5c7c645d648e083271620039fa11e827244cf1
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-09-14 23:11:12 +0530

    feat: Error handling when promise rejected

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -7,42 +7,56 @@ const prisma = new Prisma({
 
 // prisma.query prisma.mutation prisma.subscription prisma.exists
 
-// prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
-// })
+const createPostForUser = async (authorId, data) => {
+    const userExists = await prisma.exists.User({ id: authorId })
 
-// prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
-// })
-
-// prisma.mutation.createPost({
-//     data: {
-//         title: "GraphQL 101",
-//         body: "",
-//         published: false,
-//         author: {
-//             connect: {
-//                 id: "cjjybkwx5006h0822n32vw7dj"
-//             }
-//         }
-//     }
-// }, '{ id title body published }').then((data) => {
-//     console.log(data)
-//     return prisma.query.users(null, '{ id name posts { id title } }')
-// }).then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
-// })
-
-prisma.mutation.updatePost({
-    where: {
-        id: "ckezw4kgt00bd0733gts52b3e"
-    },
-    data: {
-        body: "This is how to get started with Graphql...",
-        published: true
+    if (!userExists) {
+        throw new Error('User not found')
     }
-}, '{ id }').then((data) => {
-    return prisma.query.posts(null, '{ id title body published }')
-}).then((data) => {
-    console.log(data)
-})
+
+    const post = await prisma.mutation.createPost({
+        data: {
+            ...data,
+            author: {
+                connect: {
+                    id: authorId
+                }
+            }
+        }
+    }, '{ author { id name email posts { id title published } } }')
+
+    return post.author
+}
+
+// createPostForUser('cjjybkwx5006h0822n32vw7dj', {
+//     title: 'Great books to read',
+//     body: 'The War of Art',
+//     published: true
+// }).then((user) => {
+//     console.log(JSON.stringify(user, undefined, 2))
+// }).catch((error) => {
+//     console.log(error.message)
+// })
+
+const updatePostForUser = async (postId, data) => {
+    const postExists = await prisma.exists.Post({ id: postId })
+
+    if (!postExists) {
+        throw new Error('Post not found')
+    }
+
+    const post = await prisma.mutation.updatePost({
+        where: {
+            id: postId
+        },
+        data
+    }, '{ author { id name email posts { id title published } } }')
+    
+    return post.author
+}
+
+// updatePostForUser("power", { published: true }).then((user) => {
+//     console.log(JSON.stringify(user, undefined, 2))
+// }).catch((error) => {
+//     console.log(error.message)
+// })
