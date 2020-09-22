97542b02d8011ac671483e5a9d3181700f25eea7
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-09-22 10:19:21 +0530

    feat: added authorisation token

--- graphql-prisma/src/Subscription/subscription.js
+++ graphql-prisma/src/Subscription/subscription.js
@@ -1,20 +1,28 @@
 const Subscription = {
   comment: {
-      subscribe(parent, { postId }, { db, pubsub }, info){
-          const post = db.posts.find((post) => post.id === postId && post.published)
-
-          if (!post) {
-              throw new Error('Post not found')
-          }
-
-          return pubsub.asyncIterator(`comment ${postId}`)
+      subscribe(parent, { postId }, { prisma }, info){
+        return prisma.subscription.comment({
+            where:{
+                node: {
+                    post: {
+                        id: postId
+                    }
+                }
+            }
+        })
       }
   },
   post: {
-      subscribe(parent, args, { pubsub }, info) {
-          return pubsub.asyncIterator('post')
+        subscribe(parent, { postId }, { prisma }, info){
+            return prisma.subscription.post({
+                where:{
+                    node: {
+                        published: true
+                    }
+                }
+            }, info)
+          }
       }
-  }
 }
 
 export { Subscription as default }
