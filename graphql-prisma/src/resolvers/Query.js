227cf884e492cf78a365e605c6b8f20f05e5d9ae
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-09-24 11:30:59 +0530

    Added mypost end point to find post created by the author

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -2,74 +2,91 @@ import getUserId from "../Utils/getUserId"
 
 const Query = {
   users(parent, args, { prisma }, info) {
-    const opArgs = {}
-
-    if (args.query) {
-      opArgs.where = {
-        OR: [
-          {
-            name_contains: args.query,
-          },
-          {
-            email_contains: args.query,
-          },
-        ],
+      const opArgs = {}
+      
+      if (args.query) {
+          opArgs.where = {
+              OR: [{
+                  name_contains: args.query
+              }, {
+                  email_contains: args.query
+              }]
+          }
       }
-    }
 
-    return prisma.query.users(opArgs, info)
+      return prisma.query.users(opArgs, info)
+  },
+  myPosts(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request)
+      const opArgs = {
+          where: {
+              author: {
+                  id: userId
+              }
+          }
+      }
+
+      if (args.query) {
+          opArgs.where.OR = [{
+              title_contains: args.query
+          }, {
+              body_contains: args.query
+          }]
+      }
+
+      return prisma.query.posts(opArgs, info)
   },
   posts(parent, args, { prisma }, info) {
-    const opArgs = {}
-
-    if (args.query) {
-      opArgs.where = {
-        OR: [
-          {
-            title_contains: args.query,
-          },
-          {
-            body_contains: args.query,
-          },
-        ],
+      const opArgs = {
+          where: {
+              published: true
+          }
       }
-    }
 
-    return prisma.query.posts(opArgs, info)
+      if (args.query) {
+          opArgs.where.OR = [{
+              title_contains: args.query
+          }, {
+              body_contains: args.query
+          }]
+      }
+
+      return prisma.query.posts(opArgs, info)
   },
   comments(parent, args, { prisma }, info) {
-    return prisma.query.comments(null, info)
+      return prisma.query.comments(null, info)
   },
-  me(parent, args, { prisma, request}, info) {
-    const userId = getUserId(request)
-    const user = prisma.query.user({where: {
-      id: userId
-    }})
-    console.log(userId, user)
-    if(!user){
-      throw new Error('No user found')
-    }
-    return user
+  me(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request)
+      
+      return prisma.query.user({
+          where: {
+              id: userId
+          }
+      })
   },
   async post(parent, args, { prisma, request }, info) {
-    const userId = getUserId(request, false)
-    const posts = await prisma.query.posts({
-      where: {
-        id: args.id,
-        OR:[{
-          published: true
-        },{
-          author: {
-            id: userId
+      const userId = getUserId(request, false)
+
+      const posts = await prisma.query.posts({
+          where: {
+              id: args.id,
+              OR: [{
+                  published: true
+              }, {
+                  author: {
+                      id: userId
+                  }
+              }]
           }
-        }]
+      }, info)
+
+      if (posts.length === 0) {
+          throw new Error('Post not found')
       }
-    }, info)
-    if(posts.length === 0){
-      throw new Error('No post found')
-    }
-    return posts[0]
-  },
+
+      return posts[0]
+  }
 }
 
 export { Query as default }
