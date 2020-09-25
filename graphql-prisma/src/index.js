8bd2ebce57f6062cb4f3a901372b715c7f835e49
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-09-25 10:43:47 +0530

    Implemented feagment to don't show other user private data

--- graphql-prisma/src/index.js
+++ graphql-prisma/src/index.js
@@ -1,35 +1,25 @@
 import { GraphQLServer, PubSub } from "graphql-yoga"
 import db from "./DB/db"
-import Query from "./resolvers/Query"
-import Mutation from "./resolvers/Mutation"
-import User from "./resolvers/User"
-import Post from "./resolvers/Post"
-import Comment from "./resolvers/Comment"
-import Subscription from "./Subscription/subscription"
 import prisma from "./prisma"
 // Scalar types - String, Boolean, Int, Float, ID
+import { resolvers, fragmentReplacements } from './resolvers/index'
 
 const pubsub = new PubSub()
+
 const server = new GraphQLServer({
-  typeDefs: "./src/Schema/schema.graphql",
-  resolvers: {
-    Query,
-    Mutation,
-    User,
-    Post,
-    Comment,
-    Subscription,
-  },
-  context(request) {
-    return {
-        db,
-        pubsub,
-        prisma,
-        request
-    }
-},
+    typeDefs:  "./src/Schema/schema.graphql",
+    resolvers,
+    context(request) {
+        return {
+            db,
+            pubsub,
+            prisma,
+            request
+        }
+    },
+    fragmentReplacements
 })
 
 server.start(() => {
-  console.log("The server is up!")
+    console.log('The server is up!')
 })
--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,5 +1,30 @@
+import getUserId from '../Utils/getUserId'
 const User = {
-   
+    posts: {
+        fragment: 'fragment userId on User { id }',
+        resolve(parent, args, { prisma }, info) {
+            return prisma.query.posts({
+                where: {
+                    published: true,
+                    author: {
+                        id: parent.id
+                    }
+                }
+            })
+        }
+    },
+    email: {
+        fragment: 'fragment userId on User { id }',
+        resolve(parent, args, { request }, info) {
+            const userId = getUserId(request, false)
+
+            if (userId && userId === parent.id) {
+                return parent.email
+            } else {
+                return null
+            }
+        }
+    }
 }
 
 export { User as default }
