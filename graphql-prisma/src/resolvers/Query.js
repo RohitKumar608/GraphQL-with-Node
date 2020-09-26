c844c083f0a629e7ad81f2fe36d27b00f52ebb7c
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-09-26 17:13:55 +0530

    feat: Pagination added

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -2,91 +2,121 @@ import getUserId from "../Utils/getUserId"
 
 const Query = {
   users(parent, args, { prisma }, info) {
-      const opArgs = {}
-      
-      if (args.query) {
-          opArgs.where = {
-              OR: [{
-                  name_contains: args.query
-              }, {
-                  email_contains: args.query
-              }]
-          }
-      }
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after
+    }
 
-      return prisma.query.users(opArgs, info)
+    if (args.query) {
+      opArgs.where = {
+        OR: [
+          {
+            name_contains: args.query,
+          },
+          {
+            email_contains: args.query,
+          },
+        ],
+      }
+    }
+
+    return prisma.query.users(opArgs, info)
   },
   myPosts(parent, args, { prisma, request }, info) {
-      const userId = getUserId(request)
-      const opArgs = {
-          where: {
-              author: {
-                  id: userId
-              }
-          }
-      }
+    const userId = getUserId(request)
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      where: {
+        author: {
+          id: userId,
+        },
+      },
+    }
 
-      if (args.query) {
-          opArgs.where.OR = [{
-              title_contains: args.query
-          }, {
-              body_contains: args.query
-          }]
-      }
+    if (args.query) {
+      opArgs.where.OR = [
+        {
+          title_contains: args.query,
+        },
+        {
+          body_contains: args.query,
+        },
+      ]
+    }
 
-      return prisma.query.posts(opArgs, info)
+    return prisma.query.posts(opArgs, info)
   },
   posts(parent, args, { prisma }, info) {
-      const opArgs = {
-          where: {
-              published: true
-          }
-      }
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      where: {
+        published: true,
+      },
+    }
 
-      if (args.query) {
-          opArgs.where.OR = [{
-              title_contains: args.query
-          }, {
-              body_contains: args.query
-          }]
-      }
+    if (args.query) {
+      opArgs.where.OR = [
+        {
+          title_contains: args.query,
+        },
+        {
+          body_contains: args.query,
+        },
+      ]
+    }
 
-      return prisma.query.posts(opArgs, info)
+    return prisma.query.posts(opArgs, info)
   },
   comments(parent, args, { prisma }, info) {
-      return prisma.query.comments(null, info)
+    const args = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+    }
+    return prisma.query.comments(args, info)
   },
   me(parent, args, { prisma, request }, info) {
-      const userId = getUserId(request)
-      
-      return prisma.query.user({
-          where: {
-              id: userId
-          }
-      })
+    const userId = getUserId(request)
+
+    return prisma.query.user({
+      where: {
+        id: userId,
+      },
+    })
   },
   async post(parent, args, { prisma, request }, info) {
-      const userId = getUserId(request, false)
+    const userId = getUserId(request, false)
 
-      const posts = await prisma.query.posts({
-          where: {
-              id: args.id,
-              OR: [{
-                  published: true
-              }, {
-                  author: {
-                      id: userId
-                  }
-              }]
-          }
-      }, info)
+    const posts = await prisma.query.posts(
+      {
+        where: {
+          id: args.id,
+          OR: [
+            {
+              published: true,
+            },
+            {
+              author: {
+                id: userId,
+              },
+            },
+          ],
+        },
+      },
+      info
+    )
 
-      if (posts.length === 0) {
-          throw new Error('Post not found')
-      }
+    if (posts.length === 0) {
+      throw new Error("Post not found")
+    }
 
-      return posts[0]
-  }
+    return posts[0]
+  },
 }
 
 export { Query as default }
